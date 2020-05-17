e875995e2498cb400e7b6ca7dcdbf562ad2b7655
aomonov <aomonov@freewheel.com>
2020-05-17 13:50:44 -0500

    appni qush

--- basic/app.js
+++ basic/app.js
@@ -1,1 +1,8 @@
-console.log("Hello World")
+console.log("Hello World");
+
+var http=require('http');
+http.createServer(function(req,res){
+	res.writeHead(200, {'Content-type': 'text/plain'});
+	res.end('Hello World\n');
+}).listen(8080, '127.0.0.1');
+console.log('Server running at http://127.0.01:8080');
